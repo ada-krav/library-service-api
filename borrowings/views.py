975ff0fbe401ec0f1efe87a1ea4f7ce42937999c
Bohdan Kovalenko <bog7807@gmail.com>
2023-04-24 23:11:07 +0300

    add borrowings filters for users and staff

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from django_filters import filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, generics
 from .models import Borrowing
 from .serializers import (
     BorrowingSerializer,
@@ -26,3 +28,24 @@ class BorrowingViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class BorrowingList(generics.ListCreateAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+    def get_queryset(self):
+        is_active_filter = self.request.query_params.get('is_active')
+        if is_active_filter is True:
+            return Borrowing.objects.filter(actual_return_date=None)
+
+        if not self.request.user.is_authenticated:
+            return Borrowing.objects.none()
+        elif self.request.user.is_superuser:
+            user_id = self.request.query_params.get('user_id')
+            if user_id:
+                return Borrowing.objects.filter(user=user_id)
+            else:
+                return Borrowing.objects.all()
+        else:
+            return Borrowing.objects.filter(user=self.request.user)
