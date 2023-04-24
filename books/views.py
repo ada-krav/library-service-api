a474a489cffb3672a77c205953b9aa6b669905c6
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-04-24 15:44:28 +0300

    Added permissions.py for model book/ only admin can CRUD

--- books/views.py
+++ books/views.py
@@ -1,9 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .models import Book
+from .permissions import IsAdminUser
 from .serializers import BookSerializer
 
 
 class BookViewSets(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAdminUser | permissions.IsAuthenticatedOrReadOnly]
