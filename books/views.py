21c91b4039ecd1f3e4c9f9f55c271c27dac1b890
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-04-25 11:49:29 +0300

    Fixed permissions

--- books/views.py
+++ books/views.py
@@ -1,11 +1,14 @@
 from rest_framework import viewsets, permissions
 
 from .models import Book
-from .permissions import IsAdminUser
+from .permissions import IsBookAdminOrReadOnly
 from .serializers import BookSerializer
 
 
 class BookViewSets(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAdminUser | permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsBookAdminOrReadOnly,
+    ]
