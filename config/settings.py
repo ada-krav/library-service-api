f7735678cb4e7fc0554a4b1ae549947613d93a24
manjustice <manjustice1998@gmail.com>
2023-04-25 13:00:11 +0300

    Added celery settings

--- config/settings.py
+++ config/settings.py
@@ -172,3 +172,11 @@ SIMPLE_JWT = {
 # Telegram settings
 TELEGRAM_TOKEN_API = os.getenv("TELEGRAM_TOKEN_API")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+
+# Celery settings
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_TIMEZONE = "Europe/Kyiv"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
